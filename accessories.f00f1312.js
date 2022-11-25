5b711ef3a25df99c59e6b0a27678669cc7dd31ee
vyzhimov <83233095+vyzhimov@users.noreply.github.com>
2022-11-25 05:29:21 +0000

    Deploying to gh-pages from @ vyzhimov/project11-shopforgamers@46c2de5aa64726b7d6286a2c88524a8cba549ffd 🚀

--- accessories.f00f1312.js
+++
@@ -1,2 +0,0 @@
-function filterCards(s){var e=document.querySelectorAll(".accessories-list .accessories-item"),i=s.target.dataset.filter;"*"===i?e.forEach((function(s){return s.classList.remove("visually-hidden")})):e.forEach((function(s){s.classList.contains(i)?s.classList.remove("visually-hidden"):s.classList.add("visually-hidden")}))}
-//# sourceMappingURL=accessories.f00f1312.js.map
